8b12b3ab3d2b1f5b1d980a5e7b385bf6da2f54be
Stefani Nair <stefaninairescobar@gmail.com>
2024-11-10 13:44:14 -0300

    Agregue la VistaPaqueteDetalle

---
+++ src/Vistas/VistaPaqueteDetalle.java
@@ -0,0 +1,129 @@
+
+package Vistas;
+
+import AccesoADatos.PaqueteData;
+import Entidades.Paquete;
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Stefani Escobar
+ */
+public class VistaPaqueteDetalle extends javax.swing.JInternalFrame {
+
+   private JTable table;
+   private DefaultTableModel tableModel;
+   private PaqueteData paqueteData;
+    
+    
+    public VistaPaqueteDetalle() {
+        initComponents();
+        cabecera();
+        llenarTabla();
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        jLabel1.setText("Paquetes comprados los ultimos meses:");
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration//GEN-END:variables
+
+  // Configurar modelo de la tabla
+    private void cabecera() {
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("ID Paquete");
+        tableModel.addColumn("Fecha Inicio");
+        tableModel.addColumn("Fecha Fin");
+        tableModel.addColumn("Origen");
+        tableModel.addColumn("Destino");
+        tableModel.addColumn("Cantidad Pasajeros");
+        tableModel.addColumn("Medio de Pago");
+        tableModel.addColumn("Precio Total");
+    }
+    private void llenarTabla() {
+        ArrayList<Paquete> paquetes = paqueteData.resumenPaquetesComprados();
+
+        for (Paquete paquete : paquetes) {
+            Object[] rowData = {
+                paquete.getIdPaquete(),
+                paquete.getFechaInicio(),
+                paquete.getFechaFin(),
+                paquete.getOrigen().getCiudad(),  // Suponiendo que el método getCiudad() existe en Destino
+                paquete.getDestino().getCiudad(), // Suponiendo que el método getCiudad() existe en Destino
+                paquete.getCantidadPasajeros(),
+                paquete.getMedioPago(),
+                paquete.getPrecioTotal()
+            };
+            tableModel.addRow(rowData);
+        }
+    }
+
+
+}
